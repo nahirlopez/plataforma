f96be13de6c0775cc87b2f714067c63e06f6ea7a
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-05-24 08:46:18 -0300

    Initial commit

---
+++ Assets/playercontroller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float ZSpeed;
+    public float rotationspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //transform.Translate(0, 0, ZSpeed);
+        //transform.Rotate(0, 0, ZSpeed);
+
+        // if(transform.position.z < 4.5f)
+        //{     transform.position += new Vector3(0, 0, ZSpeed); }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, ZSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -ZSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles -= new Vector3(0, rotationspeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, rotationspeed, 0);
+
+        }
+    }
+}
